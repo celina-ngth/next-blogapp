fabd7fbb165c42722668f883b2715b4b887a6083
celina-ngth <ngeth.celina@gmail.com>
2024-12-15 21:46:25 +0100

    feat: manage api errors

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,19 +3,37 @@ import { formatPosts, formatPost } from '@/utils/formatPosts'
 import { notFound } from 'next/navigation'
 
 export async function getPosts(): Promise<PostProps[]> {
-	const response = await fetch(`${process.env.API_POSTS_URL}/posts`)
+	try {
+		const response = await fetch(`${process.env.API_POSTS_URL}/posts`)
+		const data = await response.json()
 
-	const data = await response.json()
-	if (!data) notFound()
+		if (!data || !data.posts) {
+			notFound()
+		}
 
-	return formatPosts(data?.posts)
+		return formatPosts(data.posts)
+	} catch (error) {
+		console.error('Failed to fetch posts', error)
+		throw new Error('Failed to fetch posts')
+	}
 }
 
 export async function getPost(id: number): Promise<PostProps> {
-	const response = await fetch(`${process.env.API_POSTS_URL}/posts/${id}`)
+	try {
+		const response = await fetch(`${process.env.API_POSTS_URL}/posts/${id}`)
 
-	const data = await response.json()
-	if (!data) notFound()
+		if (!response.ok) {
+			throw new Error(`Failed to fetch post with id: ${id}`)
+		}
 
-	return formatPost(data)
+		const data = await response.json()
+		if (!data) {
+			notFound()
+		}
+
+		return formatPost(data)
+	} catch (error) {
+		console.error('Error fetching post:', error)
+		throw error
+	}
 }
